6816e20d87d91b70f33eb21dcb4eef9d1b6cd22b
rafaella <rafaellasantos.iosdeveloper@gmail.com>
2023-09-24 13:54:24 -0300

    Conclusao desafio

---
+++ Desafio Playground-DIO.playground/Contents.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+let constante = "Steve"
+
+var variável: String? = "Jobs"
+
+if let nome = variável{
+    print("Nome completo: \(constante) \(nome)")
+} else {
+    print("Nome completo: \(constante) Wozniak")
+}
